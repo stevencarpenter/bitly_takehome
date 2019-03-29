65c9b7501d6ffa234ef4f4caba548c01302dc94f
Steven Carpenter <steve.carpenter@ibotta.com>
2019-03-28 21:44:56 -0600

    add average clicks per country route

--- app/api.py
+++ app/api.py
@@ -1,19 +1,40 @@
-from flask import Flask
-from flask_restful import reqparse, Api, Resource
+import functools
+import operator
+from collections import Counter
 
+from bitly_api_functions import *
+from flask import Flask, jsonify
+from flask import request
+from flask_restful import Api
 
 app = Flask(__name__)
 api = Api(app)
-parser = reqparse.RequestParser()
-parser.add_argument('bitlink')
 
 
-class AverageClicksPerCountry(Resource):
-    def get(self):
-        return
+@app.errorhandler(403)
+def access_denied(error=None):
+    message = {
+        "status":  403,
+        "message": "No access token supplied. Access denied: " + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 403
 
-# add route
-api.add_resource(AverageClicksPerCountry, '/bitly/')
+    return resp
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route("/bitly/clicks/average")
+def get_average_clicks():
+    if "access_token" in request.args:
+        token = request.args["access_token"]
+        default_user_group = get_default_user_group(token)
+        bitlinks = get_bitlinks(token, default_user_group)
+        clicks_per_bitlink = [get_country_clicks(token, bitlink) for bitlink in bitlinks]
+        return jsonify(
+            average_clicks_per_country(dict(functools.reduce(operator.add, map(Counter, clicks_per_bitlink))), 30))
+    else:
+        return access_denied()
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
